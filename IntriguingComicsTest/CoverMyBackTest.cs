6ad39fe30ec1b4c9ffe9aa0c1f2e6c0a9a05e63c
n998877665544332211 <nicolas.juzda@alum.utoronto.ca>
2024-06-29 15:02:15 -0400

    Update and rename CoverMyBackTest.cs to IntriguingComicsTest/CoverMyBackTest.cs

---
+++ IntriguingComicsTest/CoverMyBackTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using IntriguingComics.Frank;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using Handelabra.Sentinels.UnitTest;
+using System.Collections.Generic;
+
+namespace IntriguingComicsTest
+{
+    [TestFixture()]
+    public class CoverMyBackTest : BaseTest
+    {
+        protected TurnTakerController frank { get { return FindVillain("Frank"); } }
+
+        [Test()]
+        public void FrankDealsDamageWhenBossDamaged()
+        {
+            SetupGameController("IntriguingComics.Frank", "Legacy", "Ra", "Megalopolis");
+
+            Card breakaway = PlayCard("Breakaway");
+
+            Card cover = PlayCard("CoverMyBack");
+
+            QuickHPStorage(legacy);
+
+            DealDamage(legacy, breakaway, 2, DamageType.Fire);
+
+            QuickHPCheck(-1);
+        }
+
+        [Test()]
+        public void FrankDoesNotDealDamageWhenNonBossDamaged()
+        {
+            SetupGameController("IntriguingComics.Frank", "Legacy", "Ra", "Megalopolis");
+
+            Card breakaway = PlayCard("Breakaway");
+
+            Card cover = PlayCard("CoverMyBack");
+
+            QuickHPStorage(legacy);
+
+            DealDamage(legacy, frank, 2, DamageType.Fire);
+
+            QuickHPCheck(0);
+        }
+
+    }
+}
