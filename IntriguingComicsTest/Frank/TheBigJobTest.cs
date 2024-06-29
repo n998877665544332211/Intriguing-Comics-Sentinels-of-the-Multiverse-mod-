2715ced8eb13a8c01e652357404412c6cf150caf
n998877665544332211 <nicolas.juzda@alum.utoronto.ca>
2024-06-29 15:05:23 -0400

    Update and rename TheBigJobTest.cs to IntriguingComicsTest/Frank/TheBigJobTest.cs

---
+++ IntriguingComicsTest/Frank/TheBigJobTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using IntriguingComics.Frank;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using Handelabra.Sentinels.UnitTest;
+using System.Collections.Generic;
+
+namespace IntriguingComicsTest
+{
+    [TestFixture()]
+    public class TheBigJobTest : BaseTest
+    {
+        protected TurnTakerController frank { get { return FindVillain("Frank"); } }
+
+        [Test()]
+        public void TheBigJobPutsOrderGangAndDeviceIntoPlay()
+        {
+            SetupGameController("IntriguingComics.Frank", "Legacy", "Ra", "Tempest", "Megalopolis");
+
+            Card outfit = PutOnDeck("AssignedOutfit");
+            Card follow = PutOnDeck("FollowMyLead");
+            Card gang = PutOnDeck("TheRestOfTheGang");
+
+            Card job = PlayCard("TheBigJob");
+
+            AssertIsInPlay(outfit);
+            AssertIsInPlay(follow);
+            AssertIsInPlay(gang);
+        }
+    }
+}
